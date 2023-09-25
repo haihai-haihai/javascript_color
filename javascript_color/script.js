2965527eed1830bda06d36900a9e593398809194
haihai-haihai <107914519+haihai-haihai@users.noreply.github.com>
2023-09-25 15:20:38 +0900

    Add files via upload

---
+++ javascript_color/script.js
@@ -0,0 +1,5 @@
+const color = document.getElementById('myColor');
+const text = document.getElementById('bodyText');
+// JavaScript
+console.log(document.getElementById('myColor').value);
+document.getElementById('bodyText').textContent = document.getElementById('myColor').value;
